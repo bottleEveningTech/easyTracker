3975345fa64e988cbd6df3eeee0de4dc0e8130ca
bottleEveningTech <tanishkasaxena2408@gmail.com>
2026-01-08 00:02:08 +0530

    New TasksModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,17 +2,14 @@ import { NgModule } from "@angular/core";
 import { AppComponent } from "./app";
 import { HeaderComponent } from "./header/header";
 import { User } from "./user/user";
-import { Task } from "./tasks/task/task";
-import { Tasks } from "./tasks/tasks";
 import { BrowserModule } from '@angular/platform-browser';
-import { NewTask } from "./tasks/new-task/new-task";
-import { FormsModule } from "@angular/forms";
 import { SharedModule } from "./shared/shared.module";
+import { TasksModule } from "./tasks/tasks.module";
 
 @NgModule({
-    declarations: [AppComponent, HeaderComponent, User, Tasks, Task, NewTask],
+    declarations: [AppComponent, HeaderComponent, User],
     bootstrap: [AppComponent],
-    imports: [BrowserModule, FormsModule, SharedModule]
+    imports: [BrowserModule, SharedModule, TasksModule]
 })
 export class AppModule {
 
